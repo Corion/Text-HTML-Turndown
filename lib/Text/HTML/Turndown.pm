98c8e3ac2a4f47ec05fc8fd7e918c4f68ac4de9e
Max Maischein <corion@corion.net>
2025-01-19 13:17:47 +0100

    Add license/repository information

--- lib/Text/HTML/Turndown.pm
+++ lib/Text/HTML/Turndown.pm
@@ -538,4 +538,30 @@ L<https://github.com/laurent22/joplin/tree/dev/packages/turndown>
 
 L<https://github.com/laurent22/joplin/tree/dev/packages/turndown-plugin-gfm>
 
+=head1 REPOSITORY
+
+The public repository of this module is
+L<https://github.com/Corion/Text-HTML-Turndown>.
+
+=head1 SUPPORT
+
+The public support forum of this module is L<https://perlmonks.org/>.
+
+=head1 BUG TRACKER
+
+Please report bugs in this module via the Github bug queue at
+L<https://github.com/Corion/Text-HTML-Turndown/issues>
+
+=head1 AUTHOR
+
+Max Maischein C<corion@cpan.org>
+
+=head1 COPYRIGHT (c)
+
+Copyright 2025- by Max Maischein C<corion@cpan.org>.
+
+=head1 LICENSE
+
+This module is released under the Artistic License 2.0.
+
 =cut
--- lib/Text/HTML/Turndown/HighlightedCodeBlock.pm
+++ lib/Text/HTML/Turndown/HighlightedCodeBlock.pm
@@ -39,3 +39,31 @@ sub install ($class, $target) {
 }
 
 1;
+
+=head1 REPOSITORY
+
+The public repository of this module is
+L<https://github.com/Corion/Text-HTML-Turndown>.
+
+=head1 SUPPORT
+
+The public support forum of this module is L<https://perlmonks.org/>.
+
+=head1 BUG TRACKER
+
+Please report bugs in this module via the Github bug queue at
+L<https://github.com/Corion/Text-HTML-Turndown/issues>
+
+=head1 AUTHOR
+
+Max Maischein C<corion@cpan.org>
+
+=head1 COPYRIGHT (c)
+
+Copyright 2025- by Max Maischein C<corion@cpan.org>.
+
+=head1 LICENSE
+
+This module is released under the Artistic License 2.0.
+
+=cut
